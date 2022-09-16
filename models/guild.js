329c6fde0f0f86933ff442b23ffffbeb4a76020a
morphe63550 <lapetitemaisonduparfum@gmail.com>
2022-09-16 02:16:31 +0200

    Add guild

--- models/guild.js
+++ models/guild.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 
 const guildSchema = mongoose.Schema({
     id: String,
-   prefix: { type: String, default: "!" },
-   logChannel: { type: String, default: "1010221231921967176" },
-   testChannel: { type: String, default: "1012704976440668292" }
+    prefix: { type: String, default: "!" },
+    logChannel: { type: String, default: "905156673491070998" },
+    ticketCategory: { type: String, default: "1015000401046679743" },
+    ticketChannel: { type: String, default: "1015562617919643759" },
+    testChannel: { type: String, default: "1020097935461064836" }
 });
 
 module.exports = mongoose.model("Guild", guildSchema);
